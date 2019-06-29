9f69097514f0c752a5124e8fe89f428d170d240d
Borys Tymiński <tyminski.borys@gmail.com>
2019-06-29 15:14:27 +0200

    add admin route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@
 
 const express = require('express')
 const router = express.Router()
+const adminRouter = require('./api/admin.route')
 const userRouter = require('./api/user.route')
 const authRouter = require('./api/auth.route')
 
 // api status
 router.get('/status', (req, res) => { res.send({status: 'OK'}) })
 
+router.use('/admin', adminRouter)
 router.use('/user', userRouter)
 router.use('/auth', authRouter)
 
