219cafb024c6fc65854175846ff0d28016fd4d8d
Borys Tymiński <tyminski.borys@gmail.com>
2019-07-03 18:05:30 +0200

    add reset and update user routes

--- src/routes/api/user.route.js
+++ src/routes/api/user.route.js
@@ -5,10 +5,15 @@ const router = express.Router()
 const userController = require('../../controllers/user.controller')
 const validator = require('express-validation')
 const { create } = require('../../validations/user.validation')
+const auth = require('../../middlewares/authorization')
 
-router.post('/register', validator(create), userController.register) // validate and register
-router.post('/login', userController.login) // login
+router.post('/register', validator(create), userController.register)
+router.post('/login', userController.login)
+router.post('/resetStart', auth(), userController.reset.sendMail)
 
 router.get('/confirm', userController.confirm)
+router.get('/resetConfirm', userController.reset.updatePass)
+
+router.put('/update', auth(), userController.update)
 
 module.exports = router
