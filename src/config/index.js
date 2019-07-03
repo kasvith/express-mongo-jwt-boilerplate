bd96df88b45e3328349b85a821250d5653754622
Borys Tymiński <tyminski.borys@gmail.com>
2019-07-03 18:05:59 +0200

    use base_uri from env in config

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,7 @@ require('dotenv').config() // load .env file
 
 module.exports = {
   port: process.env.PORT,
+  baseURI: process.env.BASE_URI,
   app: process.env.APP,
   env: process.env.NODE_ENV,
   secret: process.env.APP_SECRET,
