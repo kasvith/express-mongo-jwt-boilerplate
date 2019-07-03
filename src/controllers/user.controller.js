e1fe8edca9569356a77694b61f011d496c97b860
Borys Tymiński <tyminski.borys@gmail.com>
2019-07-03 18:04:22 +0200

    add update, reset.sendMail and reset.updatePass methods

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,9 @@ const config = require('../config')
 const httpStatus = require('http-status')
 const uuidv1 = require('uuid/v1')
 const generateToken = require('../models/utils/findAndGenerateToken')
+const passport = require('../services/passport')
+const bcrypt = require('bcrypt-nodejs')
+const transporter = require('../services/transporter')
 
 exports.register = async (req, res, next) => {
   try {
@@ -32,6 +35,21 @@ exports.login = async (req, res, next) => {
   }
 }
 
+exports.update = async (req, res, next) => {
+  try {
+    if (!passport.user) return res.status(httpStatus.FORBIDDEN)
+    const user = await User.findOne({ 'email': passport.user.email }).exec()
+    if (!user.passwordMatches(req.body.password)) return res.status(httpStatus.FORBIDDEN)
+    if (req.body.password) req.body.password = bcrypt.hashSync(req.body.password)
+    await User.findOneAndUpdate(
+      { '_id': passport.user._id },
+      { $set: req.body }
+    )
+  } catch (error) {
+    next(error)
+  }
+}
+
 exports.confirm = async (req, res, next) => {
   try {
     await User.findOneAndUpdate(
@@ -41,5 +59,47 @@ exports.confirm = async (req, res, next) => {
     return res.json({ message: 'OK' })
   } catch (error) {
     next(error)
+  }
+}
+
+exports.reset = {
+  async sendMail (req, res, next) {
+    try {
+      const resetPasswordKey = uuidv1()
+      await User.findOneAndUpdate(
+        { '_id': passport.user._id },
+        { 'resetPasswordKey': resetPasswordKey }
+      )
+      const mailOptions = {
+        from: 'noreply',
+        to: passport.user.email,
+        subject: 'Reset password',
+        html: `<div><h1>Hello user!</h1><p>Click <a href="${config.baseURI}/api/user/resetConfirm?key=${resetPasswordKey}">link</a> to reset your password.</p></div><div><h1>Hello developer!</h1><p>Feel free to change this template ;).</p></div>`
+      }
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          next(error)
+        } else {
+          return res.json({ message: 'OK' })
+        }
+      })
+    } catch (error) {
+      next(error)
+    }
+  },
+  async updatePass (req, res, next) {
+    try {
+      if (!req.query.key) return res.status(httpStatus.BAD_REQUEST)
+      const newPassword = uuidv1()
+      const newPasswordHash = bcrypt.hashSync(newPassword)
+      await User.findOneAndUpdate(
+        { 'resetPasswordKey': req.query.key },
+        { $set: { 'password': newPasswordHash, 'resetPasswordKey': '' } }
+      )
+      return res.json({ message: 'OK', newPassword: newPassword })
+    } catch (error) {
+      next(error)
+    }
   }
 }
